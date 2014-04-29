3aa905111a96199feed792e008b96dfc372034a3
serg.petrochenko <sergei9876@mail.ru>
2014-04-29 18:35:48 +0400

    Initial Commit

---
+++ WoTA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WoTA
+//
+//  Created by Sergei Petrochenko on 29.04.14.
+//  Copyright (c) 2014 Sergei Petrochenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WoTA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WoTA
+//
+//  Created by Sergei Petrochenko on 29.04.14.
+//  Copyright (c) 2014 Sergei Petrochenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
