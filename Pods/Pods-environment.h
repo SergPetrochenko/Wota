b49b0da769dae42390451e9b5dcc9408712eba0b
serg.petrochenko <sergei9876@mail.ru>
2014-04-29 16:05:28 +0400

    Первый слив
    29 апреля

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// XDKAirMenu
+#define COCOAPODS_POD_AVAILABLE_XDKAirMenu
+#define COCOAPODS_VERSION_MAJOR_XDKAirMenu 1
+#define COCOAPODS_VERSION_MINOR_XDKAirMenu 1
+#define COCOAPODS_VERSION_PATCH_XDKAirMenu 0
+
--- WoTA/ViewController.m
+++ WoTA/ViewController.m
@@ -6,24 +6,70 @@
 //  Copyright (c) 2014 Sergei Petrochenko. All rights reserved.
 //
 
+
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <XDKAirMenuDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
+
 @implementation ViewController
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.airMenuController = [XDKAirMenuController sharedMenu];
+    self.airMenuController.airDelegate = self;
+    //self.airMenuController.isMenuOnRight = TRUE;
+    
+    [self.view addSubview:self.airMenuController.view];
+    [self addChildViewController:self.airMenuController];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"TableViewSegue"])
+    {
+        self.tableView = ((UITableViewController*)segue.destinationViewController).tableView;
+    }
+}
+
+
+#pragma mark - XDKAirMenuDelegate
+
+- (UIViewController*)airMenu:(XDKAirMenuController*)airMenu viewControllerAtIndexPath:(NSIndexPath*)indexPath
+{
+    UIStoryboard *storyboard = self.storyboard;
+    UIViewController *vc = nil;
+    
+    vc.view.autoresizesSubviews = TRUE;
+    vc.view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    
+    if (indexPath.row == 0)
+        vc = [storyboard instantiateViewControllerWithIdentifier:@"ViewController1"];
+    else if (indexPath.row == 1)
+        vc = [storyboard instantiateViewControllerWithIdentifier:@"ViewController2"];
+    
+    return vc;
+}
+
+- (UITableView*)tableViewForAirMenu:(XDKAirMenuController*)airMenu
+{
+    return self.tableView;
 }
 
 @end
